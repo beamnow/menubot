fa4270af7ebe8ef93dcb217a5e5d044169dda8c2
beamnow <60428026+beamnow@users.noreply.github.com>
2022-11-08 15:06:10 +0900

    Add files via upload

--- toy_slackbot/slacktest.py
+++ toy_slackbot/slacktest.py
@@ -3,13 +3,14 @@ logging.basicConfig(level=logging.DEBUG)
 
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+from keyfile import key
 
-slack_token = token # Bot OAuth Token
+slack_token = key.slack_token # Bot OAuth Token
 client = WebClient(token=slack_token)
 
 try:
     response = client.chat_postMessage(
-        channel= Channeld ID, # Channel ID
+        channel=key.channel_id, # Channel ID
         text="say hi to Ryan! :thumbsup:"
     )
 except SlackApiError as e:
