38f97909282e78ca8d53c94d3db43e82c1f574e2
beamnow <60428026+beamnow@users.noreply.github.com>
2022-11-08 15:43:16 +0900

    edit

--- toy_slackbot/parsertest.py
+++ toy_slackbot/parsertest.py
@@ -3,20 +3,13 @@ from urllib.error import HTTPError
 import urllib.parse
 import requests
 from bs4 import BeautifulSoup
-import pprint
+from keyfile import url
 
-
-# import ssl
-# context = ssl._create_unverified_context()
 url = url
 find = ""
 
 req = requests.get(url)
 soup = BeautifulSoup(req.content, 'html.parser')
-
 finds = soup.find_all('img', class_='org_image')
-# for find in finds:
-#     find = urllib.parse.urljoin(url, find['src'])
-
 find = urllib.parse.urljoin(url, finds[1]['src'])
 print(find)
--- toy_slackbot/slacktest.py
+++ toy_slackbot/slacktest.py
@@ -3,14 +3,14 @@ logging.basicConfig(level=logging.DEBUG)
 
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
-from keyfile import key
+from keyfile import slack_token, channel_id
 
-slack_token = key.slack_token # Bot OAuth Token
+slack_token = slack_token # Bot OAuth Token
 client = WebClient(token=slack_token)
 
 try:
     response = client.chat_postMessage(
-        channel=key.channel_id, # Channel ID
+        channel=channel_id, # Channel ID
         text="say hi to Ryan! :thumbsup:"
     )
 except SlackApiError as e:
